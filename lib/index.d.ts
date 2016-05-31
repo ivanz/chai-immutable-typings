1a31afd4b676c12ec53b0784a95918865b0507ea
Ivan Zlatev <ivan@ivanz.com>
2016-05-31 15:36:35 +0100

    Added some comments.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,9 @@
 declare module 'chai-immutable' {
   import {Utils} from 'chai/lib/Utils';
 
+  // chai-immutable exports a single function (is a callable? module).
+  //
+  // The namespace+function combo enable the use of "import * as X from chai-immutable" and "use(X)" in chai.
   function initialize(chai: any,  utils: Utils) : void;
   namespace initialize {
   }
@@ -14,6 +17,7 @@ declare module '~chai/lib/Assertion' {
     (size: number, message?: string): Assertion;
   }
 
+  // Extend chai's asserts with the extra ones added by chai-immutable
   export interface Assertion {
     size: Size;
     sizeOf: Size;
